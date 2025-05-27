c61a9c3d8d1487055537519416a22a260c7f1dc6
tobi <tobi19.py@gmail.com>
2025-05-27 14:48:29 -0500

    Configure logo filepath

--- main.go
+++ main.go
@@ -8,9 +8,11 @@ import (
 func main() {
 	const filepathRoot = "."
 	const port = "8080"
+	const logoPath = "/assets/logo.png"
 
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir("filepathRoot")))
+	mux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
+	mux.Handle("assets/logo.png", http.FileServer(http.Dir(logoPath)))
 
 	srv := &http.Server{
 		Addr:    ":" + port,
