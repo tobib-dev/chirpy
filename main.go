d645227c22e9ab1574b640c68d4ac0befdee8604
tobi <tobi19.py@gmail.com>
2025-05-27 14:25:04 -0500

    Update server display message to show filepath

--- main.go
+++ main.go
@@ -6,15 +6,17 @@ import (
 )
 
 func main() {
+	const filepathRoot = "."
 	const port = "8080"
 
 	mux := http.NewServeMux()
+	mux.Handle("/", http.FileServer(http.Dir("filepathRoot")))
+
 	srv := &http.Server{
 		Addr:    ":" + port,
 		Handler: mux,
 	}
 
-	log.Printf("Serving on port: %s\n", port)
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
 	log.Fatal(srv.ListenAndServe())
 }
