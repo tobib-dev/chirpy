53dc5e843099fb3610598710ae208a77224f4fd6
tobi <tobi19.py@gmail.com>
2025-05-28 14:10:49 -0500

    Refactor readiness and reset to a separate file

--- main.go
+++ main.go
@@ -11,20 +11,9 @@ type apiConfig struct {
 	fileserverHits atomic.Int32
 }
 
-func handlerReadiness(w http.ResponseWriter, r *http.Request) {
-	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(http.StatusText(http.StatusOK)))
-}
-
 func (cfg *apiConfig) handlerWriteHits(w http.ResponseWriter, r *http.Request) {
 	msg := fmt.Sprintf("Hits: %v", cfg.fileserverHits.Load())
 	w.Write([]byte(msg))
-}
-
-func (cfg *apiConfig) handlerResetHits(w http.ResponseWriter, r *http.Request) {
-	cfg.fileserverHits.Swap(0)
-	w.Write([]byte("Successfully reset hits"))
 }
 
 func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
@@ -43,7 +32,7 @@ func main() {
 	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app/", http.FileServer(http.Dir(filepathRoot)))))
 	mux.HandleFunc("/healthz", handlerReadiness)
 	mux.HandleFunc("/metrics", apiCfg.handlerWriteHits)
-	mux.HandleFunc("/reset", apiCfg.handlerResetHits)
+	mux.HandleFunc("/reset", apiCfg.handlerReset)
 
 	srv := &http.Server{
 		Addr:    ":" + port,
