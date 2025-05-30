150c688703d8bc7223d723ac6887886361669c0f
tobi <tobi19.py@gmail.com>
2025-05-30 10:38:24 -0500

    Configure chirpy database

---
+++ sql/schema/001_users.sql
@@ -0,0 +1,10 @@
+-- +goose up
+CREATE TABLE users (
+    id UUID PRIMARY KEY,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
+    email TEXT NOT NULL UNIQUE
+);
+
+-- +goose Down
+DROP TABLE users;
