d9f70b0f00891f02b187b7ed0d5f0a5a53a8617a
tobi <tobi19.py@gmail.com>
2025-06-05 11:25:50 -0500

    Refactored get chirps into one function

--- handler_chirps_get.go
+++ handler_chirps_get.go
@@ -4,50 +4,41 @@ import (
 	"net/http"
 
 	"github.com/google/uuid"
-	"github.com/tobib-dev/chirpy/internal/database"
 )
 
 func (cfg *apiConfig) handlerGetAllChirps(w http.ResponseWriter, r *http.Request) {
-	authorID := r.URL.Query().Get("author_id")
-	if authorID == "" {
-		chirpList, err := cfg.db.GetAllChirps(r.Context())
-		if err != nil {
-			respondWithError(w, http.StatusInternalServerError, "Couldn't get all chirps", err)
-			return
-		}
-
-		res := getResponseList(chirpList)
-		respondWithJSON(w, http.StatusOK, res)
-	} else {
-		aID, err := uuid.Parse(authorID)
-		if err != nil {
-			respondWithError(w, http.StatusInternalServerError, "couldn't retrieve author id", err)
-			return
-		}
-
-		chirpList, err := cfg.db.GetAllChirpsForUser(r.Context(), aID)
-		if err != nil {
-			respondWithError(w, http.StatusNotFound, "couldn't find chirps", err)
-			return
-		}
-
-		res := getResponseList(chirpList)
-		respondWithJSON(w, http.StatusOK, res)
+	dbChirps, err := cfg.db.GetAllChirps(r.Context())
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't get all chirps", err)
+		return
 	}
-}
 
-func getResponseList(chirpList []database.Chirp) []Chirp {
-	response := make([]Chirp, len(chirpList))
-	for i, chirp := range chirpList {
-		response[i] = Chirp{
-			ID:        chirp.ID,
-			CreatedAt: chirp.CreatedAt,
-			UpdatedAt: chirp.UpdatedAt,
-			Body:      chirp.Body,
-			UserID:    chirp.UserID,
+	authorID := uuid.Nil
+	authorIDString := r.URL.Query().Get("author_id")
+	if authorIDString != "" {
+		authorID, err = uuid.Parse(authorIDString)
+		if err != nil {
+			respondWithError(w, http.StatusBadRequest, "invalid author id", err)
+			return
 		}
 	}
-	return response
+
+	chirps := []Chirp{}
+	for _, dbChirp := range dbChirps {
+		if authorID != uuid.Nil && dbChirp.UserID != authorID {
+			continue
+		}
+
+		chirps = append(chirps, Chirp{
+			ID:        dbChirp.ID,
+			CreatedAt: dbChirp.CreatedAt,
+			UpdatedAt: dbChirp.UpdatedAt,
+			Body:      dbChirp.Body,
+			UserID:    dbChirp.UserID,
+		})
+	}
+
+	respondWithJSON(w, http.StatusOK, chirps)
 }
 
 func (cfg *apiConfig) handlerGetChirp(w http.ResponseWriter, r *http.Request) {
